234cf210403e79572df9ef1474fd34d7da2fe653
VaughnVernon <vaughn@kalele.io>
2021-07-06 12:32:53 -0700

    Silence warnings.

--- src/main/java/nativebuild/NativeBuildEntryPoint.java
+++ src/main/java/nativebuild/NativeBuildEntryPoint.java
@@ -1,14 +1,23 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package nativebuild;
 
-import io.vlingo.xoom.actors.World;
-import io.vlingo.xoom.directory.client.ServiceRegistrationInfo;
+import java.util.Arrays;
+
 import org.graalvm.nativeimage.c.function.CEntryPoint;
 import org.graalvm.nativeimage.c.type.CCharPointer;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
 
-import java.util.Arrays;
+import io.vlingo.xoom.actors.World;
+import io.vlingo.xoom.directory.client.ServiceRegistrationInfo;
 
 public final class NativeBuildEntryPoint {
+  @SuppressWarnings("unused")
   @CEntryPoint(name = "Java_io_vlingo_xoom_directorynative_Native_start")
   public static int start(@CEntryPoint.IsolateThreadContext long isolateId, CCharPointer name) {
     final String nameString = CTypeConversion.toJavaString(name);
